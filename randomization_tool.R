ddad090ca0fb1d1040304b4c091a0d3fdacd3b0b
Oriol Arqués <oriol.arques@gmail.com>
2020-09-01 18:53:41 +0200

    Improvement of the code by renaming some key objects and including more comments.

--- randomization_tool.R
+++ randomization_tool.R
@@ -2,7 +2,10 @@ library(tidyverse)
 library(gridExtra)
 library(xlsx)
 
-# Read measures file
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### 1. Read the measures file  ------------------------------------------------
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
 # 9 x columns: initial_box, original_id, left_width, left_height, left_volume, right_width, right_height, right_volume, weight
 measures <- read.csv('measures.csv', header = TRUE, sep = ';')
 # Fix type of variables and commas for points as decimals
@@ -20,175 +23,158 @@ measures$sides_mean <- (measures$left_volume + measures$right_volume)/2
 # Calculate zscore of sides_mean
 measures$zscore <- (measures$sides_mean - mean(measures$sides_mean))/sd(measures$sides_mean)
 
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### 2. Define the parameters --------------------------------------------------
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 total.mice <- 20
 n.groups <- 4
 mice.grp <- total.mice / n.groups
+permut <- 10000
 
-#ggplot(measures, aes(x = box, y = left_volume ))+
-#    geom_point(position = position_jitter(0.2)) +
-#    stat_summary(fun = 'mean', geom = 'crossbar')
-
-#ggplot(measures, aes(x = 1, y = zscore))+
-#    geom_point(position = position_jitter(0.2)) +
-#    stat_summary(fun = 'mean', geom = 'crossbar')
-
-
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### 3. Remove outliers ---------------------------------------------------------
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 # Remove outliers: Order mice by absolute zscore values to be able to select 
-# the first XX mice closest to the average ------------------------------------
+# the first XX mice closest to the average
 measures <- measures %>% 
     arrange(abs(zscore)) %>% 
     slice_min(abs(zscore), n = total.mice)
 
 
-#by(data = measures, INDICES = measures$box, FUN = function(x){sd(x$sides_mean)})
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### 4. Create distribution permutations of mice -------------------------------
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
-#prova <- lapply(1:100, function(x){measures[sample(nrow(measures)),]})
+### 4.1 Create the necessary lists --------------------------------------------
+permutations_table <- list()
+permutations_mean_sd <- list()
+permutations_sd_sd <- list()
+permutations_weight_sd <- list()
+complete_permutations <- list()
+prov_table <- list() 
 
-### Crear un a llista de 1000 permutacions dels data.frames -------------------
-prova <- list()
-prova2 <- list()
-prova2_2 <- list()
-prova2_3 <- list()
-prova3 <- list()
-
-for(i in 1:10000){
-    prova[[i]] <- measures[sample(nrow(measures)),]
+# 4.2 Create a list with permutations of the tables ---------------------------
+for(i in seq_len(permut)){
+    permutations_table[[i]] <- measures[sample(nrow(measures)),]
 }
 
-x <- list()
-for(i in 1:4){
-    x[[i]] <- prova[i][[1]]$sides_mean[(mice.grp*(i-1)+1):(mice.grp*i)]
-}
-
-for(i in 1:10000){
+# 4.3 Create a list with the standard deviation of the mean of each group ----- 
+#      in each permutation
+for(i in seq_len(permut)){
     for(j in 1:4){
-        x[[j]] <- prova[i][[1]]$sides_mean[(mice.grp*(j-1)+1):(mice.grp*j)]
+        prov_table[[j]] <- permutations_table[i][[1]]$sides_mean[(mice.grp*(j-1)+1):(mice.grp*j)]
     }
-    #prova2[i] <- sd(c(mean(prova[i][[1]][[11]][1:5]), #(1:20/4)
-    #                  mean(prova[i][[1]][[11]][6:10]), #(20/4*1+1):(20/4*2)
-    #                  mean(prova[i][[1]][[11]][11:15]),
-    #                  mean(prova[i][[1]][[11]][16:20])))
-    y <- lapply(x, function(x){mean(x)})
-    prova2[i] <- sd(unlist(y))
+    prov_group.mean <- lapply(prov_table, function(prov_table){mean(prov_table)})
+    permutations_mean_sd[i] <- sd(unlist(prov_group.mean))
+}
+
+
+# 4.4 Create a list with the standard deviation of the sd of the mean of  ----- 
+#      each group in each permutation
+for(i in seq_len(permut)){
+    for(j in 1:4){
+        prov_table[[j]] <- permutations_table[i][[1]]$sides_mean[(mice.grp*(j-1)+1):(mice.grp*j)]
+    }
+    prov_group.sd <- lapply(prov_table, function(prov_table){sd(prov_table)})
+    permutations_sd_sd[i] <- sd(unlist(prov_group.sd))
 
 }
 
-for(i in 1:10000){
+# 4.5 Create a list with the standard deviation of the weight of each  ----- 
+#       group in each permutation
+for(i in seq_len(permut)){
     for(j in 1:4){
-        x[[j]] <- prova[i][[1]]$sides_mean[(mice.grp*(j-1)+1):(mice.grp*j)]
+        prov_table[[j]] <- permutations_table[i][[1]]$weight[(mice.grp*(j-1)+1):(mice.grp*j)]
     }
-    #prova2_2[i] <- sd(c(sd(prova[i][[1]][[11]][1:5]), 
-    #                  sd(prova[i][[1]][[11]][6:10]), 
-    #                  sd(prova[i][[1]][[11]][11:15]),
-    #                  sd(prova[i][[1]][[11]][16:20])))
-    z <- lapply(x, function(x){sd(x)})
-    prova2_2[i] <- sd(unlist(z))
-
-}
-
-for(i in 1:10000){
-    for(j in 1:4){
-        x[[j]] <- prova[i][[1]]$weight[(mice.grp*(j-1)+1):(mice.grp*j)]
-    }
-    #prova2_2[i] <- sd(c(sd(prova[i][[1]][[11]][1:5]), 
-    #                  sd(prova[i][[1]][[11]][6:10]), 
-    #                  sd(prova[i][[1]][[11]][11:15]),
-    #                  sd(prova[i][[1]][[11]][16:20])))
-    w <- lapply(x, function(x){mean(x)})
-    prova2_3[i] <- sd(unlist(w))
+    prova_group_weight <- lapply(prov_table, function(prov_table){mean(prov_table)})
+    permutations_weight_sd[i] <- sd(unlist(prova_group_weight))
     
 }
 
-for(i in 1:10000){
-    prova3[[i]] <- c(prova[[i]], means_sd = prova2[[i]], sd_sd = prova2_2[[i]], weight_sd = prova2_3[[i]])
+# 4.6 Join the permutations list with the standard deviation of the -----------
+#       mean/sd/weight of each group in each permutation
+for(i in seq_len(permut)){
+    complete_permutations[[i]] <- c(permutations_table[[i]], 
+                                    means_sd = permutations_mean_sd[[i]], 
+                                    sd_sd = permutations_sd_sd[[i]], 
+                                    weight_sd = permutations_weight_sd[[i]])
 }
 
 
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### 5. Get the indexes of the best ranked distribution permutations -----------
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+# get the indexes of the top 5 randomization tables with 
+# less variations in the mean of tumour size and mice weight
+
+best_rank <- order(unlist(lapply(complete_permutations, 
+                                 function(x) min(x$means_sd))))[
+                # select the top 1000 permutations with less mean_sd
+                order(unlist(lapply(complete_permutations, 
+                                    function(x) min(x$means_sd))))[1:1000] %in% 
+                # among those 2000 permutations with less weight_sd
+                order(unlist(lapply(complete_permutations, 
+                                    function(x) min(x$weight_sd))))[1:2000]][1:5]
 
 
-#min_msd <- min(unlist(lapply(prova3, function(x) min(x$means_sd))))
-#best_rand <- 0
-# Find the index of which the randomization makes the minimum 
-#for(i in 1:length(prova3)){
-#    if (prova3[i][[1]][[13]] == min_msd){
-#        print(i)
-#        best_rand <- i
-#        break
-#    }
-#}
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+### 6. Plot the best 5 distribution permutations ------------------------------
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
-# get the indexes of the top 5 randomization tables with less variations in the means
-#order(unlist(lapply(prova3, function(x) min(x$means_sd))))[1:10]
-#order(unlist(lapply(prova3, function(x) min(x$sd_sd))))[1:10]
-
-
-best_rank <- order(unlist(lapply(prova3, function(x) min(x$means_sd))))[
-                order(unlist(lapply(prova3, function(x) min(x$means_sd))))[1:1000] %in% 
-                order(unlist(lapply(prova3, function(x) min(x$weight_sd))))[1:2000]][1:5]
-
-# include the final box number to each mice ---------
-prova_plot <- data.frame(prova[6852])
-prova_plot$group_final <- c(rep('box1', 6), 
-                            rep('box2', 6),
-                            rep('box3', 6),
-                            rep('box4', 6),
-                            rep('box5', 6),
-                            rep('box6', 6))
-
-
-
-
-prova_plot<- pivot_longer(data =prova_plot[,c('group_final', 'left_volume', 'right_volume')], 
-                          cols = - group_final, 
-                          names_to = c('group'),
-                          values_to = 'volume')
-
-ggplot(prova_plot, aes(x = group_final, y = volume))+
-    geom_violin()+
-    geom_point(position = position_jitter(0.2)) +
-    stat_summary(fun = 'mean', geom = 'crossbar')
-
-
-
-## include the top 5 tables -------------------
+# create one object to plot the distribution of tumour sizes
 final_plot_volume <- list()
+# create one object to plot the distribution of mice weight
 final_plot_weight <- list()
 
+
 for(i in 1:5){
-    prova_plot <- data.frame(prova[best_rank[i]])
+    # for each of the best 5 distribution permutations: 
+    permutations_plot <- data.frame(permutations_table[best_rank[i]])
     
     # Assign box to the mice
     box_final <- 1
     count <- 0
-    for(mice in seq_along(prova_plot$initial_box)){
-        prova_plot$group_final[mice] <- paste('box', box_final)
+    # Starting by box 1
+    for(mice in seq_along(permutations_plot$initial_box)){
+        # assing the same box to the consecutive mice
+        permutations_plot$group_final[mice] <- paste('box', box_final)
         count <- count + 1
+        # when a group of mice is filled, go to the next box
         if(count == mice.grp){
             box_final <- box_final + 1
             count <- 0
         }
     }
-    #prova_plot$group_final <- c(rep('box1', 5), rep('box2', 5), rep('box3', 5),rep('box4', 5))
-    
-    #write.csv(prova_plot, file = paste('randomization',i,'.csv'))
-    write.xlsx(prova_plot, file = 'randomization.xlsx', 
+
+# Create an excel file where each of the 5 permutations  ----------------------
+#  is in a different sheet
+    write.xlsx(permutations_plot, file = 'randomization.xlsx', 
                sheetName = paste('rand',i),
                append = T)
     
+# Plot each of the 5 best distribution permutations ---------------------------    
     # select columns group_final, weight, left_volume, right_volume
-    prova_plot<- pivot_longer(data = prova_plot[,c('group_final', 'weight', 'left_volume', 'right_volume')], 
+    # and pivot to long format for plotting
+    permutations_plot<- pivot_longer(data = permutations_plot[,c('group_final', 
+                                                                 'weight', 
+                                                                 'left_volume', 
+                                                                 'right_volume')], 
                               cols = - c(group_final, weight), 
                               names_to = 'group',
                               values_to = 'volume')
     
-    final_plot_volume[[i]] <- ggplot(prova_plot, aes(x = group_final, y = volume))+
+    # Save the 5 plots of tumour sizes in an object
+    final_plot_volume[[i]] <- ggplot(permutations_plot, aes(x = group_final, y = volume))+
         geom_violin()+
         geom_point(position = position_jitter(0.2)) +
         stat_summary(fun = 'mean', geom = 'crossbar')+
         ggtitle(paste('scenario',i, 'volumes'))+
         theme_bw()
     
-    final_plot_weight[[i]] <- ggplot(prova_plot, aes(x = group_final, y = weight))+
+    # Save the 5 plots of mice weights in an object
+    final_plot_weight[[i]] <- ggplot(permutations_plot, aes(x = group_final, y = weight))+
         geom_point(col = 'orange', alpha = 0.6) +
         stat_summary(fun = 'mean', geom = 'crossbar')+
         ylim(c(15,25))+
@@ -196,9 +182,9 @@ for(i in 1:5){
         theme_bw()
 }
 
+# Plot
 final_plot_file <- c(rbind(final_plot_volume, final_plot_weight))
 final_plot_file <- marrangeGrob(grobs = final_plot_file,
                                 nrow = 2, ncol = 3)
-
 ggsave(paste0('randomization_volumes.pdf'), final_plot_file)
 
